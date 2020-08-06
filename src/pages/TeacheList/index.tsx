ac99bc6c3800f9d6abf58e94cddee6604c8c16e3
gemirson <gemirson.silva@gmail.com>
2020-08-05 21:30:59 -0300

    feature(Teacherform): create teacher  form

--- src/pages/TeacheList/index.tsx
+++ src/pages/TeacheList/index.tsx
@@ -2,35 +2,25 @@ import React from 'react'
 import './styles.css'
 import PageHeader from '../../components/PageHeader';
 import TeacherItem from '../../components/TeacherItem'
+import Input from '../../components/Input/Index';
 
 function TeacherList() {
   return (
     <div id="page-teacher-list" className="container">
       <PageHeader title="Este são os proffys disponíves.">
         <form id="search-teachers">
+          <Input name="subject" label="Materias" />
+          <Input name="week_day" label="Dia da semana" />
+          <Input name="time" label="Hora" />
 
-          <div className="input-block">
-            <label htmlFor="subject">Matérias</label>
-            <input type="text" id="subject" />
-          </div>
-
-          <div className="input-block">
-            <label htmlFor="week-day">Dia da semana</label>
-            <input type="text" id="week-day" />
-          </div>
-
-          <div className="input-block">
-            <label htmlFor="time">Hora</label>
-            <input type="text" id="time" />
-          </div>
         </form>
 
       </PageHeader>
       <main>
-       <TeacherItem/>
-       <TeacherItem/>
-       <TeacherItem/>
-       <TeacherItem/>
+        <TeacherItem />
+        <TeacherItem />
+        <TeacherItem />
+        <TeacherItem />
       </main>
     </div>
   );
--- src/pages/TeacherForm/index.tsx
+++ src/pages/TeacherForm/index.tsx
@@ -2,14 +2,51 @@
 import React from 'react';
 import PageHeader from '../../components/PageHeader';
 
+import warningIcon from '../../assets/images/icons/warning.svg'
 
+import './styles.css'
+import Input from '../../components/Input/Index';
 // import { Container } from './styles';
 
 function TeacherForm() {
   return (
 
     <div id="page-teacher-form" className="container">
-     <PageHeader title="Que incrível que você quer dar aulas."/>
+      <PageHeader
+        title="Que incrível que você quer dar aulas."
+        description="O primerio passo é preencher esse formulário de inscrição"
+      />
+
+      <main>
+        <fieldset>
+          <legend>Seus dados</legend>
+
+          <Input name="name" label="Nome completo" />
+          <Input name="avatar" label="Avatar" />
+          <Input name="whatsapp" label="Whatsapp" />
+
+        </fieldset>
+
+        <fieldset>
+          <legend>Suas aula</legend>
+          <Input name="suject" label="Materia" />
+          <Input name="cost" label="Custo da sua hora por aula" />
+
+        </fieldset>
+
+        <footer>
+          <p>
+            <img src={warningIcon} alt="Aviso importante" />
+            Importante<br />
+            Preenchar todos os dados
+          </p>
+
+          <button type="button">
+            Salvar cadastro
+          </button>
+
+        </footer>
+      </main>
     </div>
   )
 }
